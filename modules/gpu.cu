d561e54e6e840a6b2a534688e18d5e5e12478125
IritaSee <iga.narendra@gmail.com>
2024-07-29 10:38:25 +0900

    fix function header

--- modules/gpu.cu
+++ modules/gpu.cu
@@ -17,13 +17,10 @@ all modules here are optimised for GPU and slightly different than the original 
 differences are related to GPU offset calculations
 */
 
-__device__ void kernel_DoDrugSim_single(double *d_ic50, double *d_cvar, double *d_conc, double *d_herg, double *d_CONSTANTS,
-                                      double *d_STATES, double *d_STATES_cache, double *d_RATES, double *d_ALGEBRAIC,
-                                      double *d_mec_CONSTANTS, double *d_mec_STATES, double *d_mec_RATES,
-                                      double *d_mec_ALGEBRAIC, double *d_STATES_RESULT, double *d_all_states,
-                                      double *time, double *states, double *out_dt, double *cai_result, double *ina,
-                                      double *inal, double *ical, double *ito, double *ikr, double *iks, double *ik1, double *tension,
-                                      unsigned int sample_size, cipa_t *temp_result, cipa_t *cipa_result, param_t *p_param) {
+__device__ void kernel_DoDrugSim_single(double *d_ic50, double *d_cvar, double d_conc, double *d_herg, double *d_CONSTANTS, double *d_STATES, double *d_STATES_cache, double *d_RATES, 
+                                      double *d_ALGEBRAIC, double *d_mec_CONSTANTS, double *d_mec_STATES, double *d_mec_RATES, double *d_mec_ALGEBRAIC, double *time, double *states, 
+                                      double *out_dt, double *cai_result, double *ina, double *inal, double *ical, double *ito, double *ikr, double *iks, double *ik1, double *tension, double *tcurr, 
+                                      double *dt, unsigned short sample_id, unsigned int sample_size, cipa_t *temp_result, cipa_t *cipa_result, param_t *p_param) {
     unsigned long long input_counter = 0;
 
     // INIT STARTS
@@ -487,10 +484,13 @@ __device__ void kernel_DoDrugSim_single(double *d_ic50, double *d_cvar, double *
       cipa_result[sample_id].cad50 = temp_result[sample_id].cad50;
 }
 
-__global__ void kernel_DrugSimulation(double *d_ic50, double *d_cvar, double d_conc, double *d_herg, double *d_CONSTANTS, double *d_STATES, double *d_STATES_cache, double *d_RATES, 
-                                      double *d_ALGEBRAIC, double *d_mec_CONSTANTS, double *d_mec_STATES, double *d_mec_RATES, double *d_mec_ALGEBRAIC, double *time, double *states, 
-                                      double *out_dt, double *cai_result, double *ina, double *inal, double *ical, double *ito, double *ikr, double *iks, double *ik1, double *tension, double *tcurr, 
-                                      double *dt, unsigned short sample_id, unsigned int sample_size, cipa_t *temp_result, cipa_t *cipa_result, param_t *p_param) {
+__global__ void kernel_DrugSimulation(double *d_ic50, double *d_cvar, double *d_conc, double *d_herg, double *d_CONSTANTS,
+                                      double *d_STATES, double *d_STATES_cache, double *d_RATES, double *d_ALGEBRAIC,
+                                      double *d_mec_CONSTANTS, double *d_mec_STATES, double *d_mec_RATES,
+                                      double *d_mec_ALGEBRAIC, double *d_STATES_RESULT, double *d_all_states,
+                                      double *time, double *states, double *out_dt, double *cai_result, double *ina,
+                                      double *inal, double *ical, double *ito, double *ikr, double *iks, double *ik1, double *tension,
+                                      unsigned int sample_size, cipa_t *temp_result, cipa_t *cipa_result, param_t *p_param) {
     unsigned short thread_id;
     thread_id = blockIdx.x * blockDim.x + threadIdx.x;
     if(thread_id >= sample_size){
